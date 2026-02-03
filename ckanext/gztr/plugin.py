9bc609a352807628c0424a78bcbc0d0cc931bf23
rzmk <30333942+rzmk@users.noreply.github.com>
2026-02-03 18:31:59 -0500

    fix: pop spatial after to not index in SOLR

--- ckanext/gztr/plugin.py
+++ ckanext/gztr/plugin.py
@@ -115,25 +115,6 @@ class GZTRPlugin(plugins.SingletonPlugin):
             "gazetteer_validator": gztr_validators.gazetteer_validator,
         }
 
-    # IPackageController
-    # def before_dataset_search(self, search_params):
-    #     if toolkit.request and toolkit.request.path[0:8] == "/dataset":
-    #         search_params.update(
-    #             {"fq": "+dataset_type:dataset {}".format(search_params.get("fq"))}
-    #         )
-
-    #     # if toolkit.request and toolkit.request.path[0:13] == '/organization':
-    #     # search_params.update({
-    #     #'fq': '+dataset_type:dataset {}'.format( search_params.get('fq') )
-    #     # })
-
-    #     if toolkit.request and toolkit.request.path[0:6] == "/group":
-    #         search_params.update(
-    #             {"fq": "+dataset_type:dataset {}".format(search_params.get("fq"))}
-    #         )
-
-    #     return search_params
-
     def after_dataset_show(self, context, pkg_dict):
         if not pkg_dict.get("extras"):
             extras = (
@@ -173,6 +154,7 @@ class GZTRPlugin(plugins.SingletonPlugin):
                 wkt = geometry.wkt
                 print(f"WKT: {wkt}")
                 pkg_dict["spatial"] = wkt
+                pkg_dict.pop("spatial")
                 pkg_dict.pop("spatial_full")
 
             # if place_keywords:
@@ -225,22 +207,6 @@ class GZTRPlugin(plugins.SingletonPlugin):
             search_results.update(count=len(filtered_search_results))
         return search_results
 
-    def search_params(self, bbox, search_params):
-
-        bbox = self.fit_bbox(bbox)
-
-        if not search_params.get("fq_list"):
-            search_params["fq_list"] = []
-
-        spatial_query = "{{!field f=spatial}}Intersects(ENVELOPE({minx}, {maxx}, {maxy}, {miny}))"
-
-        search_params["fq_list"].append(
-            spatial_query.format(spatial_field="spatial", **bbox)
-        )
-
-        return search_params
-
-
     def normalize_bbox(self, bbox_values):
         """
         Ensures a bbox is expressed in a standard dict
@@ -280,79 +246,3 @@ class GZTRPlugin(plugins.SingletonPlugin):
             return None
 
         return bbox
-
-    def fit_bbox(self, bbox_dict):
-        """
-        Ensures that all coordinates in a bounding box
-        fall within -180, -90, 180, 90 degrees
-
-        Accepts a dict with the following keys:
-
-        {
-                "minx": -4.96,
-                "miny": 55.70,
-                "maxx": -3.78,
-                "maxy": 56.43
-            }
-
-        """
-
-        def _adjust_longitude(value):
-            if value < -180 or value > 180:
-                value = value % 360
-                if value < -180:
-                    value = 360 + value
-                elif value > 180:
-                    value = -360 + value
-            return value
-
-        def _adjust_latitude(value):
-            if value < -90 or value > 90:
-                value = value % 180
-                if value < -90:
-                    value = 180 + value
-                elif value > 90:
-                    value = -180 + value
-            return value
-
-        return {
-            "minx": _adjust_longitude(bbox_dict["minx"]),
-            "maxx": _adjust_longitude(bbox_dict["maxx"]),
-            "miny": _adjust_latitude(bbox_dict["miny"]),
-            "maxy": _adjust_latitude(bbox_dict["maxy"]),
-        }
-
-    # def after_dataset_create(self, context, pkg_dict):
-    #     if pkg_dict["type"] != "showcase" and pkg_dict["type"] != "harvest":
-    #         groups = []
-    #         if isinstance(pkg_dict.get("group", []), list):
-    #             groups = pkg_dict.get("group")
-    #         else:
-    #             groups = json.loads(pkg_dict.get("group"))
-    #         if pkg_dict.get("group", []):
-    #             for group in groups:
-    #                 _add_to_group(context, group, pkg_dict["id"])
-
-    #     _notify_admin(context, pkg_dict)
-
-    # def after_dataset_update(self, context, pkg_dict):
-    #     groups = []
-    #     if pkg_dict["type"] != "showcase" and pkg_dict["type"] != "harvest":
-    #         old_package = toolkit.get_action("package_show")(
-    #             context, {"id": pkg_dict["id"]}
-    #         )
-    #         old_groups = [group.get("name") for group in old_package.get("groups")]
-    #         current_groups = json.loads(pkg_dict.get("group", []))
-    #         rm_groups = list(set(old_groups) - set(current_groups))
-    #         if isinstance(pkg_dict.get("group", []), list):
-    #             groups = pkg_dict.get("group")
-    #         else:
-    #             groups = json.loads(pkg_dict.get("group"))
-
-    #         # if toolkit.get_endpoint()[0] in ['dataset', 'package']:
-    #         for group in groups:
-    #             _add_to_group(context, group, pkg_dict["id"])
-    #         for group in rm_groups:
-    #             _remove_from_group(context, group, pkg_dict["id"])
-
-    #     _notify_admin(context, pkg_dict)
